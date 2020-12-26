ec722b6eba41759f35b6f87238c4b33c89693ac2
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-12-26 14:55:02 -0500

    adds two sum solution using hash memo

--- src/two-sum/index.js
+++ src/two-sum/index.js
@@ -1,14 +1,13 @@
 const twoSum = (nums, target) => {
-  const memo = new Array(nums.length - 1);
-
-  for (let i = 1; i < nums.length; i++) {
-    memo[nums[i]] = i;
-  }
+  const memo = {};
 
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i];
     const idx = memo[complement];
-    if (idx && idx !== i) return [i, idx];
+
+    if (idx !== undefined) return [idx, i];
+
+    memo[nums[i]] = i;
   }
 };
 
