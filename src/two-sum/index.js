83d610c54d555c324679ffa0ba982f356227ae48
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-12-26 14:48:01 -0500

    adds two sum solution using array as memo

--- src/two-sum/index.js
+++ src/two-sum/index.js
@@ -1,8 +1,14 @@
 const twoSum = (nums, target) => {
-  for (let i = 0; i < nums.length - 1; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) return [i, j];
-    }
+  const memo = new Array(nums.length - 1);
+
+  for (let i = 1; i < nums.length; i++) {
+    memo[nums[i]] = i;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    const idx = memo[complement];
+    if (idx && idx !== i) return [i, idx];
   }
 };
 
