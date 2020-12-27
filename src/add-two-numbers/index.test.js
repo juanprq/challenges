9f32cf9289e533631938672e20ab4d9a2e0aaeeb
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-12-27 16:17:31 -0500

    adds test to include from array utilities

--- src/add-two-numbers/index.test.js
+++ src/add-two-numbers/index.test.js
@@ -1,88 +1,60 @@
 const addTwoNumbers = require('./index');
 const { ListNode } = require('./index');
 
+const fromArrayToList = (array) => {
+  let head;
+  let prev;
+  array.forEach((item) => {
+    const currentNode = new ListNode(item);
+    if (prev) {
+      prev.next = currentNode;
+    } else {
+      head = currentNode;
+    }
+
+    prev = currentNode;
+  });
+
+  return head;
+};
+
+const fromListToArray = (head) => {
+  const array = [];
+  let currentNode = head;
+  while (currentNode) {
+    array.push(currentNode.val);
+    currentNode = currentNode.next;
+  }
+
+  return array;
+}
+
 describe('addTwoNumbers', () => {
   test('it should be a function', () => {
     expect(addTwoNumbers).toBeInstanceOf(Function);
   });
 
   test('it should return 708', () => {
-    const l1 = new ListNode(
-      2,
-      new ListNode(
-        4,
-        new ListNode(
-          3,
-        ),
-      ),
-    );
-    const l2 = new ListNode(
-      5,
-      new ListNode(
-        6,
-        new ListNode(
-          4,
-        ),
-      ),
-    );
+    const l1 = fromArrayToList([2, 4, 3]);
+    const l2 = fromArrayToList([5, 6, 4]);
 
     const result = addTwoNumbers(l1, l2);
-    expect(result.val).toEqual(7);
-    expect(result.next.val).toEqual(0);
-    expect(result.next.next.val).toEqual(8);
+    expect(fromListToArray(result)).toEqual([7, 0, 8]);
    });
 
   test('it should return 0', () => {
-    const l1 = new ListNode(0);
-    const l2 = new ListNode(0);
+    const l1 = fromArrayToList([0]);
+    const l2 = fromArrayToList([0]);
 
     const result = addTwoNumbers(l1, l2);
-    expect(result.val).toEqual(0);
-    expect(result.next).toBe(null);
+    expect(fromListToArray(result)).toEqual([0]);
   });
 
   test('it should return 89990001', () => {
-    const l1 = new ListNode(
-      9,
-      new ListNode(
-        9,
-        new ListNode(
-          9,
-          new ListNode(
-            9,
-            new ListNode(
-              9,
-              new ListNode(
-                9,
-                new ListNode(
-                  9,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-    const l2 = new ListNode(
-      9,
-      new ListNode(
-        9,
-        new ListNode(
-          9,
-          new ListNode(9),
-        ),
-      ),
-    );
+    const l1 = fromArrayToList([9, 9, 9, 9, 9, 9, 9]);
+    const l2 = fromArrayToList([9, 9, 9, 9]);
 
     const result = addTwoNumbers(l1, l2);
-    [8,9,9,9,0,0,0,1]
-    expect(result.val).toEqual(8);
-    expect(result.next.val).toBe(9);
-    expect(result.next.next.val).toBe(9);
-    expect(result.next.next.next.val).toBe(9);
-    expect(result.next.next.next.next.val).toBe(0);
-    expect(result.next.next.next.next.next.val).toBe(0);
-    expect(result.next.next.next.next.next.next.val).toBe(0);
-    expect(result.next.next.next.next.next.next.next.val).toBe(1);
+    expect(fromListToArray(result)).toEqual([8, 9, 9, 9, 0, 0, 0, 1]);
   });
 });
