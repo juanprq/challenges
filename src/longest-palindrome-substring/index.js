9255e21cdc75d46ea7e92be523132f0785fc201f
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-12-29 15:28:28 -0500

    adds solution for palindrome recursive

--- src/longest-palindrome-substring/index.js
+++ src/longest-palindrome-substring/index.js
@@ -1,17 +1,15 @@
-const isPalindrome = s => {
-  return s
-    .split('')
-    .every((char, i) => char === s.charAt(s.length - i - 1));
-}
-
 const longestPalindrome = (string) => {
   let longest = '';
 
+  const isPalindrome = (i, j) => {
+    if (i >= j) return true;
+    return string.charAt(i) === string.charAt(j) && isPalindrome(i + 1, j - 1);
+  }
+
   for (let i = 0; i < string.length; i++) {
-    for (let j = i + 1; j < string.length + 1; j++) {
-      const substring = string.slice(i, j);
-      if (isPalindrome(substring) && substring.length > longest.length) {
-        longest = substring;
+    for (let j = i; j < string.length; j++) {
+      if (isPalindrome(i, j) && (j - i + 1) > longest.length) {
+        longest = string.slice(i, j + 1);
       }
     }
   }
