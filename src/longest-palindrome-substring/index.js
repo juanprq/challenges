87c296d3e9da2a4f6f7309e78d8620b38d9f6681
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-12-29 18:30:35 -0500

    adds palindrome solution around a center

--- src/longest-palindrome-substring/index.js
+++ src/longest-palindrome-substring/index.js
@@ -1,34 +1,31 @@
 const longestPalindrome = (string) => {
-  let longest = '';
-  const memo = [];
+  if ((string || '').length === 0) return '';
+  const expandAroundCenter = (i, j) => {
+    let left = i;
+    let right = j;
 
+    while (left >= 0 && right < string.length && string.charAt(left) === string.charAt(right)) {
+      left--;
+      right++;
+    }
+
+    return right - left - 1;
+  };
+
+  let start = 0;
+  let end = 0;
   for (let i = 0; i < string.length; i++) {
-    for (let j = 0; j < string.length; j++) {
-      if (memo[i]) {
-        memo[i][j] = false;
-      } else {
-        memo[i] = [false];
-      }
+    const l1 = expandAroundCenter(i, i);
+    const l2 = expandAroundCenter(i, i + 1);
+
+    const length = Math.max(l1, l2);
+    if (length > (end - start)) {
+      start = i - Math.floor((length - 1) / 2);
+      end = i + Math.floor(length / 2);
     }
   }
 
-  const isPalindrome = (i, j) => {
-    if (memo[i][j]) return memo[i][j]
-    if (i >= j) return true;
-    const result = string.charAt(i) === string.charAt(j) && isPalindrome(i + 1, j - 1);
-    memo[i][j] = result;
-    return result;
-  }
-
-  for (let i = 0; i < string.length; i++) {
-    for (let j = i; j < string.length; j++) {
-      if (isPalindrome(i, j) && (j - i + 1) > longest.length) {
-        longest = string.slice(i, j + 1);
-      }
-    }
-  }
-
-  return longest;
+  return string.slice(start, end + 1);
 };
 
 module.exports = longestPalindrome;
