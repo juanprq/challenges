05431f1aabb0b01d1e06e2a85bf13c59d312251f
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-12-30 13:02:49 -0500

    improving validation of overflow on reverse integer solution

--- src/reverse-integer/index.js
+++ src/reverse-integer/index.js
@@ -1,3 +1,6 @@
+const MAX = Math.pow(2, 31) - 1;
+const MIN = -1 * Math.pow(2, 31);
+
 const reverseInteger = (number) => {
   let negative = number < 0 ? -1 : 1;
   let reminder = number * negative;
@@ -5,11 +8,13 @@ const reverseInteger = (number) => {
 
   while (reminder > 0) {
     const digit = reminder % 10;
+    if (result > MAX / 10 || (result === MAX / 10 && digit > 7)) return 0;
+    if (result < MIN / 10 || (result === MIN / 10) && digit < -8) return 0;
+
     result = result * 10 + digit;
     reminder = Math.floor(reminder / 10);
   }
 
-  if (result < - Math.pow(2, 31) || result > Math.pow(2, 31) - 1) return 0;
   return result * negative;
 };
 
