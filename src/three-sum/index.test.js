85e8b07d089ebc09f008cdbff091d041dbc9fe91
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-01 17:17:05 -0500

    add test suit for the three-sum problem

---
+++ src/three-sum/index.test.js
@@ -0,0 +1,20 @@
+const threeSum = require('./index');
+
+describe('threeSum', () => {
+  it('should be a function', () => {
+    expect(threeSum).toBeInstanceOf(Function);
+  });
+
+  it('should return [[-1, -1, 2], [-1, 0, 1]]', () => {
+    const input = [-1, 0, 1, 2, -1, -4];
+    expect(threeSum(input)).toEqual([[-1, -1, 2], [-1, 0, 1]]);
+  });
+
+  it('should return []', () => {
+    expect(threeSum([])).toEqual([]);
+  });
+
+  it('should return []', () => {
+    expect(threeSum([0])).toEqual([]);
+  });
+});
