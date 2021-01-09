e01a5299aec08f8bad980fd1eed6595f0a183b02
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-09 16:50:05 -0500

    adds remove-duplicates test suit

---
+++ src/remove-duplicates/index.test.js
@@ -0,0 +1,19 @@
+const removeDuplicates = require('./index');
+
+describe('removeDuplicates', () => {
+  it('should be a function', () => {
+    expect(removeDuplicates).toBeInstanceOf(Function);
+  });
+
+  it('should return 2', () => {
+    const nums = [1, 1, 2];
+    expect(removeDuplicates(nums)).toEqual(2);
+    expect(nums).toEqual([1, 2]);
+  });
+
+  it('should return 5', () => {
+    const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+    expect(removeDuplicates(nums)).toEqual(5);
+    expect(nums).toEqual([0, 1, 2, 3, 4]);
+  });
+});
