ec87537ef22e9ce169eda7c473b9705a4ded0d7a
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-08 19:30:21 -0500

    adds test suit for merge-k-sorted-list

---
+++ src/merge-k-sorted-list/index.test.js
@@ -0,0 +1,57 @@
+const mergeKList = require('./index');
+
+const arrayToList = (array) => {
+  let head;
+  let current;
+
+  array.forEach(element => {
+    if (head) {
+      current.next = new ListNode(element);
+      current = current.next;
+    } else {
+      head = new ListNode(element);
+      current = head;
+    }
+  });
+
+  return head;
+};
+
+const listToArray = (head) => {
+  let current = head;
+  const result = [];
+
+  while(current) {
+    result.push(current.val);
+    current = current.next;
+  }
+
+  return result;
+};
+
+describe('mergeKList', () => {
+  it('should be a function', () => {
+    expect(mergeKList).toBeInstance(Function);
+  });
+
+  it('should return [1, 1, 2, 3, 4, 4, 5, 6]', () => {
+    const input = [
+      arrayToList([1, 4, 5]),
+      arrayToList([1, 2, 3]),
+      arrayToList([2, 6]),
+    ];
+    const result = [1, 1, 2, 3, 4, 4, 5, 6];
+
+    expect(listToArray(mergeKList(input))).toEqual(result);
+  });
+
+  it('should return []', () => {
+    const input = [];
+    expect(listToArray(mergeKList(input))).toEqual([]);
+  });
+
+  it('should return []', () => {
+    const input = [null];
+    expect(listToArray(mergeKList(input))).toEqual([]);
+  });
+});
