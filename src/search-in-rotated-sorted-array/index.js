b481c5b56790d00d8655983503c009f9c18b8db5
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-12 19:35:04 -0500

    try to resolve search in rotated sorted array

---
+++ src/search-in-rotated-sorted-array/index.js
@@ -0,0 +1,30 @@
+const search = (array, target) => {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left < right) {
+    const mid = Math.floor((right - left) / 2 + left);
+    if (array[mid] === target) return mid;
+
+    // determine the sorted part
+    if (array[left] <= array[mid]) {
+      // sorted left
+      if (target >= array[left] && target < array[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      // sorted right
+      if (target > array[mid] && target <= array[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+};
+
+module.exports = search;
