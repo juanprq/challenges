1ca661d09b45e7d7ddacdddcd0ba18373d30c777
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-16 13:11:50 -0500

    adds test suit for first-missing-positive problem

---
+++ src/first-missing-positive/index.test.js
@@ -0,0 +1,23 @@
+const firstMissingPositive = require('./index');
+
+describe('firstMissingPositive', () => {
+  it('should be a function', () => {
+    expect(firstMissingPositive).toBeInstanceOf(Function);
+  });
+
+  it('should return 3', () => {
+    const nums = [1, 2, 0];
+    expect(firstMissingPositive(nums)).toEqual(3);
+  });
+
+  it('should return 2', () => {
+    const nums = [3, 4, -1, 1];
+    expect(firstMissingPositive(nums)).toEqual(2);
+  });
+
+  it('should return 1', () => {
+    const nums = [7, 8, 9, 11, 12];
+;
+    expect(firstMissingPositive(nums)).toEqual(1);
+  });
+});
