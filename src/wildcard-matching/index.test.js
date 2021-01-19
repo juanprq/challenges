02dd7b929f22ecc2fbc5b1362fdddecbf8a9c4c6
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-18 20:38:53 -0500

    adds test suit for wildcard matching

---
+++ src/wildcard-matching/index.test.js
@@ -0,0 +1,42 @@
+const isMatch = require('./index');
+
+describe('isMatch', () => {
+  it('should be a function', () => {
+    expect(isMatch).toBeInstanceOf(Function);
+  });
+
+  it('should return false', () => {
+    const s = 'aa';
+    const p = 'a';
+
+    expect(isMatch(s, p)).toBe(false);
+  });
+
+  it('should return true', () => {
+    const s = 'aa';
+    const p = '*';
+
+    expect(isMatch(s, p)).toBe(true);
+  });
+
+  it('should return false', () => {
+    const s = 'cb';
+    const p = '?a';
+
+    expect(isMatch(s, p)).toBe(false);
+  });
+
+  it('should return true', () => {
+    const s = 'adceb';
+    const p = '*a*b';
+
+    expect(isMatch(s, p)).toBe(true);
+  });
+
+  it('should return false', () => {
+    const s = 'acdcb';
+    const p = 'a*c?b';
+
+    expect(isMatch(s, p)).toBe(false);
+  });
+});
