096350dfafba0603b2d68c87eca1c1b9ced928df
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-19 20:12:24 -0500

    adds solution with memo

--- src/wildcard-matching/index.js
+++ src/wildcard-matching/index.js
@@ -1,19 +1,28 @@
 const isMatch = (s, p) => {
+  const memo = [];
 
   const match = (sI = 0, pI = 0) => {
+    if (memo[sI] && memo[sI][pI] !== undefined) return memo[sI][pI];
+
     if (sI === s.length && pI === p.length) return true;
     if (sI > s.length || pI > p.length) return false;
 
     const currentChar = s.charAt(sI);
     const currentPattern = p.charAt(pI);
 
+    let result;
     if (currentPattern === '?') {
-      return currentChar !== undefined && match(sI + 1, pI + 1);
+      result = currentChar !== undefined && match(sI + 1, pI + 1);
     } else if (p[pI] === '*') {
-      return match(sI + 1, pI) || match(sI, pI + 1);
+      result = match(sI + 1, pI) || match(sI, pI + 1);
     } else {
-      return s[sI] === p[pI] && match(sI + 1, pI + 1);
+      result = s[sI] === p[pI] && match(sI + 1, pI + 1);
     }
+
+    if (!memo[sI]) memo[sI] = [];
+    memo[sI][pI] = result;
+
+    return result;
   }
 
   return match();
