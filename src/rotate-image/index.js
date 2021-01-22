a7a9df897b715dc981cf10af0b458dc409dfc595
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-22 14:46:35 -0500

    adds test for the conpcept of iterating over the borders

--- src/rotate-image/index.js
+++ src/rotate-image/index.js
@@ -1,18 +1,42 @@
 const rotate = (matrix) => {
   const n = matrix.length;
-  // let's tackle the problem first using additional memory
-  const newMatrix = new Array(n);
-  for (let i = 0; i < n; i++) {
-    newMatrix[i] = new Array(n);
-  }
 
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      newMatrix[j][n - i - 1] = matrix[i][j];
+  for (let i = 0; i < n - 2; i++) {
+    for (let j = 0; j < n - 1; j++) {
+      let aux;
+      let aux2;
+
+      // son 4 iteraciones, por que? una por cada borde
+      let source = [i, j];
+      let target = [j, n - 1 - i];
+
+      aux = matrix[target[0]][target[1]];
+      matrix[target[0]][target[1]] = matrix[source[0]][source[1]];
+
+      target = [n - 1 - i, n - 1 - j];
+
+      aux2 = aux;
+      aux = matrix[target[0]][target[1]];
+      matrix[target[0]][target[1]] = aux2;
+
+      target = [n - 1 - j, i];
+
+      aux2 = aux;
+      aux = matrix[target[0]][target[1]];
+      matrix[target[0]][target[1]] = aux2;
+
+      target = [i, j];
+
+      aux2 = aux;
+      aux = matrix[target[0]][target[1]];
+      matrix[target[0]][target[1]] = aux2;
     }
   }
 
-  return newMatrix;
+  return matrix;
+
+  // en un matriz 3x3, solo tendria que iterar el border y de 0, 1, por que?
+  // luego debo tener cada una de las posiciones
 };
 
 module.exports = rotate;
