842b47759aa27aa0ddc7b24c1cfdbe8360bba1c8
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-23 15:19:51 -0500

    adds solution for rotate-image problem

--- src/rotate-image/index.js
+++ src/rotate-image/index.js
@@ -1,12 +1,11 @@
 const rotate = (matrix) => {
   const n = matrix.length;
-
-  for (let i = 0; i < n - 2; i++) {
-    for (let j = 0; j < n - 1; j++) {
+  let i = 0;
+  while (i <= n - i - 1) {
+    for (let j = i; j < n - i - 1; j++) {
       let aux;
       let aux2;
 
-      // son 4 iteraciones, por que? una por cada borde
       let source = [i, j];
       let target = [j, n - 1 - i];
 
@@ -31,12 +30,11 @@ const rotate = (matrix) => {
       aux = matrix[target[0]][target[1]];
       matrix[target[0]][target[1]] = aux2;
     }
+
+    i++;
   }
 
   return matrix;
-
-  // en un matriz 3x3, solo tendria que iterar el border y de 0, 1, por que?
-  // luego debo tener cada una de las posiciones
 };
 
 module.exports = rotate;
