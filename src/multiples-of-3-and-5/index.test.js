0d1e50dbb886ccdaa7ed0044348dfa8a2015439a
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-24 18:19:07 -0500

    adds more examples to the multiples-of-3-and-5 problem

--- src/multiples-of-3-and-5/index.test.js
+++ src/multiples-of-3-and-5/index.test.js
@@ -8,5 +8,22 @@ describe('multiplesOf3And5', () => {
   it('should return 23', () => {
     expect(multiplesOf3And5(10)).toEqual(23);
   });
+
+  it('should return 543', () => {
+    expect(multiplesOf3And5(49)).toEqual(543);
+  });
+
+  it('should return 233268', () => {
+    expect(multiplesOf3And5(1000)).toEqual(233168);
+  });
+
+  it('should return 16687353', () => {
+    expect(multiplesOf3And5(8456)).toEqual(16687353);
+  });
+
+  it('should return 89301283', () => {
+    expect(multiplesOf3And5(19564)).toEqual(89301183);
+  });
+
 });
 
