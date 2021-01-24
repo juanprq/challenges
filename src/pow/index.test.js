081c8eae3d6403b2f615d46520cd0342132403cd
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-24 14:07:26 -0500

    adds test suit for the pow problem

---
+++ src/pow/index.test.js
@@ -0,0 +1,19 @@
+const myPow = require('./index');
+
+describe('myPow', () => {
+  it('should be a function', () => {
+    expect(myPow).toBeInstanceOf(Function);
+  });
+
+  it('should return 1024.00000', () => {
+    expect(myPow(2.0, 10)).toEqual(1024.0);
+  });
+
+  it('should return 9.26100', () => {
+    expect(myPow(2.1, 3)).toEqual(9.26100);
+  });
+
+  it('should return ', () => {
+    expect(myPow(2.0, -2)).toEqual(0.25);
+  });
+});
