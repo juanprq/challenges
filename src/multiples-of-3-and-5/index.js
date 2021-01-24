ae9952ca73f703cf96cd4ccc31b9a7d4097e3c79
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-24 18:19:37 -0500

    fix solution for the multiples of 3 and 5 problem

--- src/multiples-of-3-and-5/index.js
+++ src/multiples-of-3-and-5/index.js
@@ -1,10 +1,9 @@
 const multiplesOf3And5 = (n) => {
   let sum = 0;
-  for (let i = 3; i < n; i += 3) {
-    sum += i;
-  }
-  for (let i = 5; i < n; i += 5) {
-    sum += i;
+  for (let i = 1; i < n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
   }
 
   return sum;
