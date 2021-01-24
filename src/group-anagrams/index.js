566d204b8c965827eda3c250c59ffe42e428a5ca
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-01-24 14:07:04 -0500

    adds sorting solution for the group anagrams problem

--- src/group-anagrams/index.js
+++ src/group-anagrams/index.js
@@ -1,47 +1,21 @@
-const countChars = (str) =>
-  str
-    .split('')
-    .reduce((accum, char) => ({
-      ...accum,
-      [char]: accum[char] ? accum[char] + 1 : 1,
-    }), {});
-
-const isAnagram = (dict1, dict2) => {
-  if (Object.keys(dict1).length !== Object.keys(dict2).length) return false;
-  return Object
-    .keys(dict1)
-    .every(key => dict1[key] === dict2[key]);
-};
-
 const groupAnagrams = (array) => {
   const result = array
-    .map((chars) => ({
-      value: chars,
-      count: countChars(chars),
-    }))
-    .reduce((accum, { value, count }) => {
-      let added = false;
-      for (let i = 0; i < accum.length; i++) {
-        const { count: groupCount } = accum[i];
-        if (isAnagram(count, groupCount)) {
-          accum[i].values.push(value);
-          added = true;
-          break;
-        }
-      }
+    .reduce((accum, string) => {
+      const sortedString = string
+        .split('')
+        .sort()
+        .join('');
 
-      if (!added) {
-        accum.push({
-          count,
-          values: [value],
-        });
+      if (accum[sortedString]) {
+        accum[sortedString].push(string);
+      } else {
+        accum[sortedString] = [string];
       }
 
       return accum;
-    }, [])
-    .map(result => result.values);
+    }, {});
 
-  return result;
+  return Object.values(result);
 };
 
 module.exports = groupAnagrams;
