96ec02f8df0ba9c0c8e912e4e1d4cbfd300bfe05
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-03 15:37:56 -0500

    adds one more test to the selection sort test suit

--- src/selection-sort/index.test.js
+++ src/selection-sort/index.test.js
@@ -20,4 +20,46 @@ describe('selectionSort', () => {
   it('should sort an array', () => {
     expect(selectionSort([3, 42, 2, 1, 39])).toEqual([1, 2, 3, 39, 42]);
   });
+
+  it('should sort a long array', () => {
+    expect(
+      selectionSort([
+        1,
+        4,
+        2,
+        8,
+        345,
+        123,
+        43,
+        32,
+        5643,
+        63,
+        123,
+        43,
+        2,
+        55,
+        1,
+        234,
+        92,
+      ])
+    ).toEqual([
+        1,
+        1,
+        2,
+        2,
+        4,
+        8,
+        32,
+        43,
+        43,
+        55,
+        63,
+        92,
+        123,
+        123,
+        234,
+        345,
+        5643,
+    ]);
+  });
 });
