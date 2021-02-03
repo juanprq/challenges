6e3564b7d2a9860d2275c9eab69ab1785a7c0089
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-03 15:31:02 -0500

    adds naive selection sort implementation

---
+++ src/selection-sort/index.js
@@ -0,0 +1,17 @@
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    let minIndex = i + 1;
+
+    for (let j = i + 2; j < array.length; j++) {
+      if (array[j] < array[minIndex]) minIndex = j;
+    }
+
+    const aux = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = aux;
+  }
+
+  return array;
+};
+
+module.exports = selectionSort;
