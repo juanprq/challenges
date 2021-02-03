f44c0b2fc95f35be30668b5d14114019af707078
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-03 15:38:19 -0500

    fix problem with the selecion sort

--- src/selection-sort/index.js
+++ src/selection-sort/index.js
@@ -1,8 +1,8 @@
 const selectionSort = (array) => {
   for (let i = 0; i < array.length - 1; i++) {
-    let minIndex = i + 1;
+    let minIndex = i;
 
-    for (let j = i + 2; j < array.length; j++) {
+    for (let j = i + 1; j < array.length; j++) {
       if (array[j] < array[minIndex]) minIndex = j;
     }
 
