56a41b2acb4a847c1419ae430e2aa674e5ca6da8
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-04 15:58:05 -0500

    adds naive solution for the insertion sort

---
+++ src/insertion-sort/index.js
@@ -0,0 +1,17 @@
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const current = array[i];
+
+    let j = i - 1;
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = current;
+  }
+
+  return array;
+};
+
+module.exports = insertionSort;
