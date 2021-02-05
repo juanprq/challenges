064f43fbdb87aab89d608c3fe9b2fc6c95079b10
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-05 15:20:10 -0500

    adds solution for the two other parts of the problem

--- src/array-digits/index.js
+++ src/array-digits/index.js
@@ -17,6 +17,46 @@ const addOne = (digits) => {
   return digits;
 };
 
+const addX = (digits, x) => {
+  let sum = x;
+  let i = digits.length - 1;
+
+  while (sum !== 0 && i >= 0) {
+    const current = digits[i] + sum;
+
+    digits[i] = current % 10;
+    sum = Math.floor(current / 10);
+    i--;
+  }
+
+  if (sum > 0) {
+    return [sum, ...digits];
+  }
+
+  return digits;
+};
+
+const add = (digitsA, digitsB) => {
+  digitsA.reverse();
+  digitsB.reverse();
+
+  const n = Math.max(digitsA.length, digitsB.length);
+  const result = new Array(n);
+  let carry = 0;
+
+  for (let i = 0; i < n; i++) {
+    const current = (digitsA[i] || 0) + (digitsB[i] || 0) + carry;
+    result[i] = current % 10;
+    carry = Math.floor(current / 10);
+  }
+
+  if (carry > 0) result.push(carry);
+
+  return result.reverse();
+};
+
 module.exports = {
   addOne,
+  addX,
+  add,
 };
