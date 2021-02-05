64372c6024ac6d2a5ea105bfd55fe4e78343baa8
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-05 15:19:52 -0500

    adds more tests to array digits problem

--- src/array-digits/index.test.js
+++ src/array-digits/index.test.js
@@ -26,4 +26,48 @@ describe('arrayDigits', () => {
       expect(addOne([9, 9, 9])).toEqual([1, 0, 0, 0]);
     });
   });
+
+  describe('addX', () => {
+    it('should be a function', () => {
+      expect(addX).toBeInstanceOf(Function);
+    });
+
+    it('should return a [1]', () => {
+      expect(addX([0], 1)).toEqual([1]);
+    });
+
+    it('should return a [1, 7]', () => {
+      expect(addX([1, 2], 5)).toEqual([1, 7]);
+    });
+
+    it('should return a [2]', () => {
+      expect(addX([1, 8 , 9], 9)).toEqual([1, 9, 8]);
+    });
+
+    it('should return a [1, 0, 0, 0]', () => {
+      expect(addX([9, 9, 9], 9)).toEqual([1, 0, 0, 8]);
+    });
+  });
+
+  describe('add', () => {
+    it('should be a function', () => {
+      expect(add).toBeInstanceOf(Function);
+    });
+
+    it('should return [5]', () => {
+      expect(add([2], [3])).toEqual([5]);
+    });
+
+    it('should return [2]', () => {
+      expect(add([2], [])).toEqual([2]);
+    });
+
+    it('should return [4, 2, 3]', () => {
+      expect(add([2, 1, 2], [2, 1, 1])).toEqual([4, 2, 3]);
+    });
+
+    it('should return [1, 4, 2, 4, 8]', () => {
+      expect(add([9, 9, 9, 9], [4, 2, 4, 9])).toEqual([1, 4, 2, 4, 8]);
+    });
+  });
 });
