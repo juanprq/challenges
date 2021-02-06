0961efdc6d18aaac6568125c8726d7699cc4ad47
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-06 11:03:35 -0500

    adds more test for logic on stack

--- src/class-stack/index.test.js
+++ src/class-stack/index.test.js
@@ -26,4 +26,44 @@ describe('stack', () => {
   it('should have a isEmpty method', () => {
     expect(stack.isEmpty).toBeInstanceOf(Function);
   });
+
+  it('should have a clear method', () => {
+    expect(stack.clear).toBeInstanceOf(Function);
+  });
+
+  it('should return the top element #peek', () => {
+    stack.push(1);
+    stack.push(2);
+
+    expect(stack.peek()).toEqual(2);
+    expect(stack.peek()).toEqual(2);
+
+    stack.pop();
+    expect(stack.peek()).toEqual(1);
+  });
+
+  it('should return the top element #pop', () => {
+    stack.push(1);
+    stack.push(2);
+
+    expect(stack.pop()).toEqual(2);
+    expect(stack.pop()).toEqual(1);
+    expect(stack.isEmpty()).toBe(true);
+  });
+
+  it('should return true if stack is empty', () => {
+    expect(stack.isEmpty()).toBe(true);
+    stack.push(1);
+    expect(stack.isEmpty()).toBe(false);
+  });
+
+
+  it('should clear all elements in stack', () => {
+    stack.push(1);
+    stack.push(2);
+
+    expect(stack.isEmpty()).toBe(false);
+    stack.clear();
+    expect(stack.isEmpty()).toBe(true);
+  });
 });
