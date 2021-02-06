32795d02204ec2cad71d80e9363fa81032933f68
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-06 10:56:05 -0500

    adds test suit for Stack class

---
+++ src/class-stack/index.test.js
@@ -0,0 +1,29 @@
+const Stack = require('./index');
+
+describe('stack', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it('should be an instance of Stack', () => {
+    expect(stack).toBeInstanceOf(Stack);
+  });
+
+  it('should have a push method', () => {
+    expect(stack.push).toBeInstanceOf(Function);
+  });
+
+  it('should have a pop method', () => {
+    expect(stack.pop).toBeInstanceOf(pop);
+  });
+
+  it('should have a peek method', () => {
+    expect(stack.peek).toBeInstanceOf(Function);
+  });
+
+  it('should have a isEmpty method', () => {
+    expect(stack.isEmpty).toBeInstanceOf(Function);
+  });
+});
