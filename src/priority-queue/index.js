af42fda8296c89353c59abce923fb077f9b5f005
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-08 14:34:26 -0500

    adds naive implementation for the priority queue class

--- src/priority-queue/index.js
+++ src/priority-queue/index.js
@@ -1,22 +1,44 @@
 class PriorityQueue {
-  enqueue = ([item, priority]) => {
+  collection = [];
 
+  enqueue = ([item, priority]) => {
+    this.collection.push([item, priority]);
   }
 
   dequeue = () => {
+    const frontItem = this.front();
+    let frontItemIndex;
+    for (let i = 0; i < this.collection.length; i++) {
+      if (this.collection[i][0] === frontItem) {
+        frontItemIndex = i;
+        break;
+      }
+    }
 
+    if (frontItemIndex !== undefined) {
+      this.collection = this.collection.slice(0, frontItemIndex).concat(this.collection.slice(frontItemIndex + 1));
+    }
+
+    return frontItem;
   }
 
   size = () => {
-
+    return this.collection.length;
   }
 
   front = () => {
+    const [item] = this.collection
+      .reduce((minPriority, current) => {
+        if (current[1] < minPriority[1]) return current;
+        return minPriority;
+      });
 
+    return item;
   }
 
-  isEmpty = () => {
 
+  isEmpty = () => {
+    return this.size() === 0;
   }
 }
 
