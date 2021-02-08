b561837a6a9bafa87b5ec855185879c1bf06963e
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-08 13:56:28 -0500

    adds implementation for the first priority queue tests

---
+++ src/priority-queue/index.js
@@ -0,0 +1,23 @@
+class PriorityQueue {
+  enqueue = ([item, priority]) => {
+
+  }
+
+  dequeue = () => {
+
+  }
+
+  size = () => {
+
+  }
+
+  front = () => {
+
+  }
+
+  isEmpty = () => {
+
+  }
+}
+
+module.exports = PriorityQueue;
