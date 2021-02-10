9706b149c58e7b55b2455e7b2b30413e73763f90
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-10 15:34:31 -0500

    adds test suit for the set class

---
+++ src/set/index.test.js
@@ -0,0 +1,76 @@
+const Set = require('./index');
+
+describe('Set', () => {
+  let set;
+
+  beforeEach(() => {
+    set = new Set();
+  });
+
+  it('should be a Set', () => {
+    expect(set).toBeInstanceOf(Set);
+  });
+
+  it('should have the add method', () => {
+    expect(set.add).toBeInstanceOf(Function);
+  });
+
+  it('should have values method', () => {
+    expect(set.values).toBeInstanceOf(Function);
+  });
+
+  it('should have the remove method', () => {
+    expect(set.remove).toBeInstanceOf(Function);
+  });
+
+  it('should have a size method', () => {
+    expect(set.size).toBeInstanceOf(Function);
+  });
+
+  it('should have a has method', () => {
+    expect(set.has).toBeInstanceOf(Function);
+  })
+
+  it('should not add duplicate values', () => {
+    set.add(1);
+    set.add(1);
+
+    expect(set.values()).toEqual([1]);
+  });
+
+  it('should return true when an item is successfully added', () => {
+    expect(set.add(1)).toBe(true);
+  });
+
+  it('should return false when an item is duplicated', () => {
+    set.add(1);
+    expect(set.add(1)).toBe(false);
+  });
+
+
+  it('should remove only items present', () => {
+    set.add(1);
+    set.add(2);
+
+    expect(set.remove(1)).toBe(true);
+    expect(set.remove(3)).toBe(false);
+  });
+
+  it('should remove the item from the set', () => {
+    set.add(1);
+    set.add(2)
+
+    set.remove(1);
+    expect(set.values()).toEqual([2]);
+  });
+
+
+  it('should return the correct size of the set', () => {
+    set.add(1);
+    set.add(1);
+    set.add(2);
+    set.add(3);
+
+    expect(set.size()).toEqual(3);
+  });
+});
