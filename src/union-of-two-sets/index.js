3efe5b8a19c1020834bb0c036e0ca64cadb7ea69
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-11 15:12:49 -0500

    adds implementation of the union of two sets

---
+++ src/union-of-two-sets/index.js
@@ -0,0 +1,50 @@
+class Set {
+  constructor() {
+    this.dictionary = {};
+    this.length = 0;
+  }
+
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+
+  values() {
+    return Object.values(this.dictionary);
+  }
+
+  add(element) {
+    if (!this.has(element)) {
+      this.dictionary[element] = element;
+      this.length++;
+      return true;
+    }
+
+    return false;
+  }
+
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
+
+    return false;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  union(set) {
+    const result = new Set();
+    const add = item => result.add(item);
+
+    this.values().forEach(add);
+    set.values().forEach(add);
+
+    return result;
+  }
+}
+
+module.exports = Set;
