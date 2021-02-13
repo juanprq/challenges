5aecf08c6b6a983bf5e782598b96a00bb5de8cca
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-13 17:05:58 -0500

    adds test suit for the linked list class

---
+++ src/linked-list/index.test.js
@@ -0,0 +1,40 @@
+const LinkedList = require('./index');
+
+describe(LinkedList, () => {
+  let linkedList;
+  beforeEach(() => {
+    linkedList = new LinkedList();
+  });
+
+  it('should be a LinkedList', () => {
+    expect(linkedList).toBeInstanceOf(LinkedList);
+  });
+
+  it('should have an add method', () => {
+    expect(linkedList.add).toBeInstanceOf(Function);
+  });
+
+  it('should have a head method', () => {
+    expect(linkedList.head).toBeInstanceOf(Function);
+  });
+
+  it('should have a size method', () => {
+    expect(linkedList.size).toBeInstanceOf(Function);
+  });
+
+  it('should assign head to the first node added',  () => {
+    linkedList.add(1);
+    linkedList.add(2);
+
+    expect(linkedList.head()).toEqual(1);
+  });
+
+  it('should count the correct ammount of nodes', () => {
+    linkedList.add(1);
+    expect(linkedList.size()).toEqual(1);
+
+    linkedList.add(2);
+    linkedList.add(3);
+    expect(linkedList.size()).toEqual(3);
+  });
+});
