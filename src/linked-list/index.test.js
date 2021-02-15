43b83a0298eeac7eb529c005f27cb6f6896c90c0
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-15 15:50:28 -0500

    adds test suit for the isEmpty, indexOf and element at on linked-list

--- src/linked-list/index.test.js
+++ src/linked-list/index.test.js
@@ -81,4 +81,61 @@ describe(LinkedList, () => {
     expect(linkedList.head().next.next.element).toEqual(3);
     expect(linkedList.head().next.next.next).toBe(null);
   });
+
+  it('should have an isEmpty method', () => {
+    expect(linkedList.isEmpty).toBeInstanceOf(Function);
+  });
+
+
+  it('should have an indexOf method', () => {
+    expect(linkedList.indexOf).toBeInstanceOf(Function);
+  });
+
+  it('should have elementAt method', () => {
+    expect(linkedList.elementAt).toBeInstanceOf(Function);
+  });
+
+  it('should return false when there are elements in the list', () => {
+    linkedList.add(1);
+    linkedList.add(2);
+    linkedList.add(3);
+
+    expect(linkedList.isEmpty()).toBe(false);
+  });
+
+  it('should return true when there are no elements in the list', () => {
+    expect(linkedList.isEmpty()).toBe(true);
+  });
+
+  it('should return the index of a given element', () => {
+    linkedList.add(1);
+    linkedList.add(2);
+    linkedList.add(3);
+
+    expect(linkedList.indexOf(2)).toEqual(1);
+  });
+
+  it('should return -1 if the element is not found', () => {
+    linkedList.add(1);
+    linkedList.add(2);
+    linkedList.add(3);
+
+    expect(linkedList.indexOf(7)).toEqual(-1);
+  });
+
+  it('should return the element at given index', () => {
+    linkedList.add(1);
+    linkedList.add(2);
+    linkedList.add(3);
+
+    expect(linkedList.elementAt(2)).toEqual(3);
+  });
+
+  it('should return undefined if the item is not found', () => {
+    linkedList.add(1);
+    linkedList.add(2);
+    linkedList.add(3);
+
+    expect(linkedList.elementAt(3)).toBeUndefined();
+  });
 });
