5cbcd0e0b4ea054cafe8d17c46f13a0ca2c18657
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-02-23 16:30:02 -0500

    adds test suit for balanced methods on binary search tree

--- src/binary-search-tree/index.test.js
+++ src/binary-search-tree/index.test.js
@@ -81,7 +81,65 @@ describe('BinarySearchTree', () => {
     expect(binarySearchTree.isPresent(2)).toBe(true);
   });
 
-  // it('should return false with an empty tree', () => {
-  //   expect(binarySearchTree.isPresent(5)).toBe(false);
-  // });
+  it('should return false with an empty tree', () => {
+    expect(binarySearchTree.isPresent(5)).toBe(false);
+  });
+
+  it('should have the findMinHeight method', () => {
+    expect(binarySearchTree.findMinHeight).toBeInstanceOf(Function);
+  });
+
+  it('should have the findMaxHeight method', () => {
+    expect(binarySearchTree.findMaxHeight).toBeInstanceOf(Function);
+  });
+
+  it('should have the isBalancedTree', () => {
+    expect(binarySearchTree.isBalanced).toBeInstanceOf(Function);
+  });
+
+  it('should return the minimum height of the tree', () => {
+    binarySearchTree.add(2);
+    binarySearchTree.add(1);
+    binarySearchTree.add(3);
+    binarySearchTree.add(4);
+    binarySearchTree.add(5);
+    binarySearchTree.add(6);
+
+    expect(binarySearchTree.findMinHeight()).toEqual(1);
+  });
+
+  it('should return the maximum height of the tree', () => {
+    binarySearchTree.add(2);
+    binarySearchTree.add(1);
+    binarySearchTree.add(3);
+    binarySearchTree.add(4);
+    binarySearchTree.add(5);
+    binarySearchTree.add(6);
+
+    expect(binarySearchTree.findMaxHeight()).toEqual(4);
+  });
+
+  it('should return -1 if is an empty tree', () => {
+    expect(binarySearchTree.findMaxHeight()).toEqual(-1);
+    expect(binarySearchTree.findMinHeight()).toEqual(-1);
+  });
+
+  it('should return false if the tree is unbalanced', () => {
+    binarySearchTree.add(2);
+    binarySearchTree.add(1);
+    binarySearchTree.add(3);
+    binarySearchTree.add(4);
+    binarySearchTree.add(5);
+    binarySearchTree.add(6);
+
+    expect(binarySearchTree.isBalanced()).toBe(false);
+  });
+
+  it('should return true if the tree is balanced', () => {
+    binarySearchTree.add(2);
+    binarySearchTree.add(1);
+    binarySearchTree.add(3);
+
+    expect(binarySearchTree.isBalanced()).toBe(true);
+  });
 });
