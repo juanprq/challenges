1f26069b555daf90e8babe7e1eafa00f84c77fd9
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-03-03 14:21:33 -0500

    adds test suit for the remove method on the max-heap

--- src/max-heap/index.test.js
+++ src/max-heap/index.test.js
@@ -27,4 +27,19 @@ describe('MaxHeap', () => {
     const result = [31, 30, 28, 20];
     expect(maxHeap.print()).toEqual(result);
   });
+
+  it('should have a method called remove', () => {
+    expect(maxHeap.remove).toBeInstanceOf(Function);
+  });
+
+  it('should remove the gratest element of the heap while maintaining the mx heap property', () => {
+    maxHeap.insert(30);
+    maxHeap.insert(20);
+    maxHeap.insert(28);
+    maxHeap.insert(31);
+
+    expect(maxHeap.remove()).toEqual(31);
+    expect(maxHeap.remove()).toEqual(30);
+    expect(maxHeap.print()).toEqual([28, 20]);
+  });
 });
