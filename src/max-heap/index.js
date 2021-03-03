6344570d417daddbbe8e0d3412f4867ab4147902
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-03-03 14:21:55 -0500

    adds implementation for the remove on the max-heap

--- src/max-heap/index.js
+++ src/max-heap/index.js
@@ -16,6 +16,32 @@ class MaxHeap {
     }
   }
 
+  remove() {
+    const result = this.values[1];
+    this.values[1] = this.values[this.values.length - 1];
+    this.values.pop();
+
+    let index = 1;
+    while (
+      index < this.values.length - 1
+      && (
+        this.values[index * 2] > this.values[index]
+        || this.values[index * 2 + 1] > this.values[index]
+      )
+    ) {
+      const aux = this.values[index];
+      let childIndex = index * 2;
+      if (this.values[childIndex] < this.values[childIndex + 1]) childIndex++;
+
+      this.values[index] = this.values[childIndex];
+      this.values[childIndex] = aux;
+
+      index = childIndex;
+    }
+
+    return result;
+  }
+
   print() {
     return this.values.slice(1);
   }
