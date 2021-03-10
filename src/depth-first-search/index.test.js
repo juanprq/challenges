b346871746e81a79b175885f2486bed53831c283
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-03-10 18:54:59 -0500

    adds test suit for the depth first search

---
+++ src/depth-first-search/index.test.js
@@ -0,0 +1,103 @@
+const dfs = require('./index');
+
+describe('depthFirstSearch', () => {
+  it('should be a function', () => {
+    expect(dfs).toBeInstanceOf(Function);
+  });
+
+  it('should return the reachable nodes', () => {
+    const graph = [
+      [0, 1, 0, 0],
+      [1, 0, 1, 0],
+      [0, 1, 0, 1],
+      [0, 0, 1, 0],
+    ];
+    const root = 1;
+
+    expect(dfs(graph, root)).toEqual([0, 1, 2, 3]);
+  });
+
+  it('should return the reachable nodes', () => {
+    const graph = [
+      [0, 1, 0, 0],
+      [1, 0, 1, 0],
+      [0, 1, 0, 1],
+      [0, 0, 1, 0],
+    ];
+    const root = 1;
+
+    expect(dfs(graph, root)).toHaveLength(4);
+  });
+
+  it('should return the reachable nodes', () => {
+    const graph = [
+      [0, 1, 0, 0],
+      [1, 0, 1, 0],
+      [0, 1, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    const root = 3;
+
+    expect(dfs(graph, root)).toEqual([3]);
+  });
+
+  it('should return the reachable nodes', () => {
+    const graph = [
+      [0, 1, 0, 0],
+      [1, 0, 1, 0],
+      [0, 1, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    const root = 3;
+
+    expect(dfs(graph, root)).toHaveLength(1);
+  });
+
+  it('should return the reachable nodes', () => {
+    const graph = [
+      [0, 1, 0, 0],
+      [1, 0, 0, 0],
+      [0, 0, 0, 1],
+      [0, 0, 1, 0],
+    ];
+    const root = 3;
+
+    expect(dfs(graph, root)).toEqual([2, 3]);
+  });
+
+  it('should return the reachable nodes', () => {
+    const graph = [
+      [0, 1, 0, 0],
+      [1, 0, 0, 0],
+      [0, 0, 0, 1],
+      [0, 0, 1, 0],
+    ];
+    const root = 3;
+
+    expect(dfs(graph, root)).toHaveLength(2);
+  });
+
+  it('should return the reachable nodes', () => {
+    const graph = [
+      [0, 1, 0, 0],
+      [1, 0, 0, 0],
+      [0, 0, 0, 1],
+      [0, 0, 1, 0],
+    ];
+    const root = 0;
+
+    expect(dfs(graph, root)).toEqual([0, 1]);
+  });
+
+  it('should return the reachable nodes', () => {
+    const graph = [
+      [0, 1, 0, 0],
+      [1, 0, 0, 0],
+      [0, 0, 0, 1],
+      [0, 0, 1, 0],
+    ];
+    const root = 0;
+
+    expect(dfs(graph, root)).toHaveLength(2);
+  });
+});
