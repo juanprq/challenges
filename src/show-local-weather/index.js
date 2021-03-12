217d69ce05381453c2e1637dbd398807cef312e1
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-03-12 13:41:18 -0500

    adds logic to display the temperature

--- src/show-local-weather/index.js
+++ src/show-local-weather/index.js
@@ -1,9 +1,46 @@
-const showPosition = (position) => {
+const showPosition = async (position) => {
+  let isCelsius = true;
   const { latitude, longitude } = position.coords;
   const url = `https://weather-proxy.freecodecamp.rocks/api/current?lat=${latitude}&lon=${longitude}`
 
-  console.log(url);
-};
+  const { data } = await axios.get(url);
+  const {
+    weather: [{ icon }],
+    sys: { country },
+    main: { temp },
+    name,
+  } = data;
+
+  document
+    .getElementById('loading')
+    .remove();
+
+  const cardEl = document.getElementById('card');
+
+  const cityEl = document.createElement('div');
+  cityEl.appendChild(document.createTextNode(`${name}, ${country}`));
+  cardEl.appendChild(cityEl);
+
+  const temperatureEl = document.createElement('div');
+  temperatureEl.setAttribute('id', 'temperature');
+  temperatureEl.appendChild(document.createTextNode(`${temp} °C`));
+  cardEl.appendChild(temperatureEl);
+
+  const imageEl = document.createElement('img');
+  imageEl.setAttribute('src', icon);
+  cardEl.appendChild(imageEl);
+
+  const button = document.getElementById('toggle');
+  button.onclick = () => {
+    isCelsius = !isCelsius;
+
+    const text = isCelsius
+      ? `${temp} °C`
+      : `${temp * 1.8 + 32} °F`;
+
+    temperatureEl.firstChild.nodeValue = text;
+  };
+}
 
 navigator
   .geolocation
