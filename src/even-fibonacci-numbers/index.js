dac60f287fe0bf213b80ec5c1a6a3c8b62777d71
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-03-14 15:30:36 -0500

    adds improvements for memory management

--- src/even-fibonacci-numbers/index.js
+++ src/even-fibonacci-numbers/index.js
@@ -1,18 +1,17 @@
 const fiboEvenSum = (n) => {
-  const fibonacci = [1, 1];
+  let result = 0;
+  const fibonacci = [0, 1];
   while (fibonacci[fibonacci.length - 1] <= n) {
-    const lastIndex = fibonacci.length - 1;
-    fibonacci.push(fibonacci[lastIndex] + fibonacci[lastIndex - 1]);
+    const newNumber = fibonacci[1] + fibonacci[0];
+    if (newNumber % 2 === 0) {
+      result += newNumber;
+    }
+
+    fibonacci[0] = fibonacci[1];
+    fibonacci[1] = newNumber;
   }
 
-  return fibonacci
-    .reduce((accum, value) => {
-      if (value % 2 === 0) {
-        return accum + value;
-      }
-
-      return accum;
-    }, 0);
+  return result;
 };
 
 module.exports = fiboEvenSum;
