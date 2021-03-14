8fcd2acf10401c4e11b42be8de20a96ff18149e9
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-03-14 15:23:03 -0500

    solving again the multiples of 3 and 5 problem

--- src/multiples-of-3-and-5/index.js
+++ src/multiples-of-3-and-5/index.js
@@ -1,12 +1,12 @@
 const multiplesOf3And5 = (n) => {
-  let sum = 0;
-  for (let i = 1; i < n; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
+  let result = 0;
+  for (let i = 0; i < n; i++) {
+    if (i % 3  === 0 || i % 5 === 0) {
+      result += i;
     }
   }
 
-  return sum;
+  return result;
 };
 
 module.exports = multiplesOf3And5;
