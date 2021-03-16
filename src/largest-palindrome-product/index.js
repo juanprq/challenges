af953cd630648d926a4d8bac2aa13853f2126668
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-03-16 18:40:08 -0500

    adds largest palindrome product naive solution

--- src/largest-palindrome-product/index.js
+++ src/largest-palindrome-product/index.js
@@ -1,5 +1,25 @@
+const isPalindrome = (string) => {
+  for (let i = 0; i < Math.floor(string.length / 2); i++) {
+    const counterIndex = string.length - i - 1;
+
+    if (string.charAt(i) !== string.charAt(counterIndex)) return false;
+  }
+
+  return true;
+};
+
 const largestPalindromeProduct = (n) => {
-  return 0;
+  const max = Math.pow(10, n) - 1;
+
+  for (let i = max * max; i > 9; i--) {
+    if (isPalindrome(`${i}`)) {
+      for (let j = max; j > 9; j--) {
+        if (i % j === 0 && i / j <= max) return i;
+      }
+    }
+  }
+
+  return 9;
 };
 
 module.exports = largestPalindromeProduct;
