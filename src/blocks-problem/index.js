f459ac9ae846c7ecb5a80019724299d8a0cb9366
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-03-17 15:00:23 -0500

    adds naive solution to the blocks problem

---
+++ src/blocks-problem/index.js
@@ -0,0 +1,62 @@
+const blocks = [
+  {
+    gym: false,
+    school: true,
+    store: false,
+  },
+  {
+    gym: true,
+    school: false,
+    store: false,
+  },
+  {
+    gym: true,
+    school: true,
+    store: false,
+  },
+  {
+    gym: false,
+    school: true,
+    store: false,
+  },
+  {
+    gym: false,
+    school: true,
+    store: true,
+  },
+];
+
+const importantBuildings = ['gym', 'school', 'store'];
+
+const findNearestBuilding = (blocks, building, currentIndex) => {
+  const min = blocks.reduce((min, b, i) => {
+    if (b[building]) {
+      const current = Math.abs(i - currentIndex)
+      return current < min ? current : min;
+    };
+    return min;
+  }, Infinity);
+
+  return min;
+};
+
+const selectAppartment = (blocks, importantBuildings) => {
+  const buildingDistances = blocks
+    .map((block, i) => {
+      return importantBuildings
+        .map(building => findNearestBuilding(blocks, building, i))
+        .reduce((a, b) => a + b);
+    }, 0);
+
+  const { i } = buildingDistances
+    .reduce((min, distance, i) => {
+      if (!min) return { i, distance };
+      const { i: minI, distance: minDistance } = min;
+
+      return distance < minDistance ? { i, distance } : min;
+    }, null);
+
+  return i;
+};
+
+console.log(selectAppartment(blocks, importantBuildings));
