c834cea5feaeae4946fe523ed46990f88427f660
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-03-23 10:56:47 -0500

    refactor a little bit the combination-sum solution

--- src/combination-sum/index.js
+++ src/combination-sum/index.js
@@ -1,20 +1,15 @@
 const combinationSum = (candidates, target) => {
-  const result = [];
-
   const func = (currentIndex = 0, currentCandidates = [], currentSum = 0) => {
-    if (currentSum === target) {
-      result.push(currentCandidates);
-      return;
-    }
-    if (currentSum > target || currentIndex >= candidates.length) return;
+    if (currentSum === target) return [currentCandidates];
+    if (currentSum > target || currentIndex >= candidates.length) return [];
 
     const current = candidates[currentIndex];
-    func(currentIndex, [...currentCandidates, current], currentSum + current);
-    func(currentIndex + 1, currentCandidates, currentSum);
+    return [
+      ...func(currentIndex, [...currentCandidates, current], currentSum + current),
+      ...func(currentIndex + 1, currentCandidates, currentSum),
+    ];
   };
-  func();
-
-  return result;
+  return func();
 };
 
 module.exports = combinationSum;
