696c7d182727bc0716878b383385b5abdc0d1e02
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-03-30 09:34:17 -0500

    adds test suit for the spiral matrix problem

---
+++ src/spiral-matrix/index.test.js
@@ -0,0 +1,29 @@
+const spiralOrder = require('./index');
+
+describe('spiralOrder', () => {
+  it('should be a function', () => {
+    expect(spiralOrder).toBeInstanceOf(Function);
+  });
+
+  it('should return spiral order', () => {
+    const input = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    const output = [1, 2, 3, 6, 9, 8, 7, 4, 5];
+
+    expect(spiralOrder(input)).toEqual(output);
+  });
+
+  it('should return spiral order', () => {
+    const input = [
+      [1, 2, 3, 4],
+      [5, 6, 7, 8],
+      [9, 10, 11, 12],
+    ];
+    const output = [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7];
+
+    expect(spiralOrder(input)).toEqual(output);
+  });
+});
