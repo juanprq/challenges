913dd2bda6459eec1accef63e224d4670fc539ca
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-03-31 11:19:45 -0500

    adds another solution for the spiral matrix problem

--- src/spiral-matrix/index.js
+++ src/spiral-matrix/index.js
@@ -1,28 +1,38 @@
 const spiralOrder = (input) => {
+  let result = [];
+
   const n = input.length;
   const m = input[0].length;
-  const result = [];
 
   let start = 0;
-  let rowsEnd = n - 1;
-  let colsEnd = m - 1;
-  while (start <= Math.max(rowsEnd, colsEnd)) {
-    for (let i = start; i <= colsEnd; i++) {
+  let maxRow = n - 1;
+  let maxCol = m - 1;
+
+  while (start <= Math.min(maxCol, maxRow)) {
+    if (start === maxRow) {
+      result = result.concat(input[start].slice(start, maxCol + 1));
+      break;
+    }
+
+    for(let i = start; i <= maxCol; i++) {
       result.push(input[start][i]);
     }
-    for (let i = start + 1; i < rowsEnd; i++) {
-      result.push(input[i][colsEnd]);
+
+    for(let i = start + 1; i <= maxRow; i++) {
+      result.push(input[i][maxCol]);
     }
-    for (let i = colsEnd; i > start; i--) {
-      result.push(input[rowsEnd][i]);
+
+    for (let i = maxCol - 1; i >= start; i--) {
+      result.push(input[maxRow][i]);
     }
-    for (let i = rowsEnd; i > start; i--) {
+
+    for (let i = maxRow - 1; i > start; i--) {
       result.push(input[i][start]);
     }
 
     start++;
-    colsEnd--;
-    rowsEnd--;
+    maxRow--;
+    maxCol--;
   }
 
   return result;
