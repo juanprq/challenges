2fe06f2efef7f3a2e014ddf4164569aaed7377c3
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-04-19 14:06:58 -0500

    adds solution using a n square approach

--- src/move-zeroes/index.js
+++ src/move-zeroes/index.js
@@ -1,8 +1,14 @@
 const moveZeroes = (input) => {
-  const zeroes = input.filter(v => v === 0);
-  const nonZeroes = input.filter(v => v !== 0)
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === 0) {
+      for (let j = i; j < input.length - 1; j++) {
+        input[j] = input[j + 1];
+        input[j + 1] = 0
+      }
+    }
+  }
 
-  return [...nonZeroes, ...zeroes];
+  return input;
 };
 
 module.exports = moveZeroes;
