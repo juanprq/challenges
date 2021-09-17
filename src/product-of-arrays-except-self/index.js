4ee6aa8a5f598e7b2dc4a36a5109f34c867f78d1
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-09-16 22:54:51 -0500

    adds solution for product of arrays except of self in O(n)

--- src/product-of-arrays-except-self/index.js
+++ src/product-of-arrays-except-self/index.js
@@ -1,18 +1,28 @@
 const productExceptSelf = (input) => {
-  const result = [];
-  for (let i = 0; i < input.length; i++) {
-    let product = 1;
-    for (let j = 0; j < input.length; j++) {
-      if (j !== i) {
-        product *= input[j];
-      }
-    }
+  let headProduct = 1;
+  const heads = [headProduct];
 
-    if (product === -0) product = 0;
-    result.push(product);
+  // for of heads
+  for (let i = 0; i < input.length - 1; i++) {
+    headProduct *= input[i];
+    heads.push(headProduct);
   }
 
-  return result;
+  // for tails
+  let tailProduct = 1;
+  const tails = [tailProduct]
+
+  for (let i = input.length - 1; i > 0; i--) {
+    tailProduct *= input[i];
+    tails.push(tailProduct);
+  }
+
+  return heads.map((head, i) => {
+    let product = head * tails[tails.length - i - 1]
+    if (product === -0) product = 0;
+
+    return product;
+  });
 };
 
 module.exports = productExceptSelf;
