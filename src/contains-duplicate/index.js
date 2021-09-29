f49a14ded4e3150a24e225bd3c921f91a2bde9fc
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-09-28 22:17:04 -0500

    adds trick with a set for the contains-duplicate problem

--- src/contains-duplicate/index.js
+++ src/contains-duplicate/index.js
@@ -1,14 +1,6 @@
 // this solution is order of N
 const containsDuplicate = (array) => {
-  const cache = {};
-  for (let i = 0; i < array.length; i++) {
-    const value = array[i];
-
-    if (cache[value]) return true;
-    cache[value] = true;
-  }
-
-  return false;
+  return array.length !== new Set(array).size;
 };
 
 module.exports = containsDuplicate;
