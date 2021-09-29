8a5658c35f25c68593a4b73c3bd6775a657b4b11
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-09-28 22:42:26 -0500

    adds improvement to time complexity to the product except self problem

--- src/product-of-arrays-except-self/index.js
+++ src/product-of-arrays-except-self/index.js
@@ -1,25 +1,19 @@
 const productExceptSelf = (input) => {
-  let headProduct = 1;
-  const heads = [headProduct];
+    const result = []
+    let left = 1;
+    let right = 1;
 
-  let tailProduct = 1;
-  const tails = [tailProduct]
+    for (let i = 0; i < input.length; i++) {
+        result[i] = left;
+        left = left * input[i];
+    }
 
-  // for of heads
-  for (let i = 0; i < input.length - 1; i++) {
-    headProduct *= input[i];
-    heads.push(headProduct);
+    for (let i = input.length - 1; i >= 0; i--) {
+        result[i] = result[i] * right;
+        right = right * input[i];
+    }
 
-    tailProduct *= input[input.length - i - 1];
-    tails.push(tailProduct);
-  }
-
-  return heads.map((head, i) => {
-    let product = head * tails[tails.length - i - 1]
-    if (product === -0) product = 0;
-
-    return product;
-  });
+    return result;
 };
 
 module.exports = productExceptSelf;
